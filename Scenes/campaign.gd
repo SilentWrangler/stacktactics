9f3721df1304b48b5dae36aa934d9249b7b0d5f4
SilentWrangler <thousandgoblins@gmail.com>
2024-09-06 18:38:22 +0300

    small visual update for campaign mode

--- Scenes/campaign.gd
+++ Scenes/campaign.gd
@@ -8,6 +8,8 @@ var nodeList: Dictionary
 
 @export var player_location: StringName
 
+@onready var player_sprite = $PlayerSprite
+
 const VANGUARD_SIZE = 4
 
 func unlockNodes(node_ids: Array[StringName]):
@@ -20,9 +22,10 @@ func unlockNodes(node_ids: Array[StringName]):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	move_player(player_location,false)
 	if BattleData.from_battle:
 		unlockNodes(BattleData.unlocked_nodes)
-		player_location = BattleData.node_id
+		move_player(BattleData.node_id, false)
 		vanguard = BattleData.player_vanguard
 		reserve = BattleData.player_reserve
 		BattleData.from_battle = false
@@ -47,8 +50,14 @@ func giveRewards(rewards: Rewards):
 		else:
 			reserve.append(unit)
 
-func move_player(to: StringName):
+func move_player(to: StringName, tween: bool = true):
 	player_location=to
+	var target_node = nodeList[to]
+	if tween:
+		var t = player_sprite.create_tween()
+		t.tween_property(player_sprite,"position",target_node.position,1.0)
+	else:
+		player_sprite.position = target_node.position
 
 func persist_unlocked():
 	BattleData.unlocked_nodes.clear()
